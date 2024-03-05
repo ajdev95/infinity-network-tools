b849d5428f61b1342af8cadafa6746f626494afc
AJ <87986054+ajdev95@users.noreply.github.com>
2024-03-05 04:45:54 +0300

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -10,6 +10,10 @@ def check_user_profile(username):
         response = requests.get(f"https://in8.network/users/{username}")
 
         if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            description0 = soup.find('p', class_='card-text')
+            description = description0.text.strip() if description0 and description0.text.strip() else "No description set"
+
             balance = parse_balance(response.text)
             has_infinity_founder = parse_badge(response.text, "crown")
             has_staff = parse_badge(response.text, "Staff")
@@ -17,7 +21,7 @@ def check_user_profile(username):
             has_bug_hunter = parse_badge(response.text, "bugHunter")
             has_premium = parse_badge(response.text, "Premium")
 
-            print(f"{Fore.LIGHTBLACK_EX}{current_time}{Fore.RESET} {Fore.LIGHTGREEN_EX}<+>{Fore.RESET} {Fore.RESET}{Fore.LIGHTWHITE_EX}User's info:{Fore.RESET} {Fore.LIGHTBLACK_EX}username={Fore.RESET}{Fore.LIGHTWHITE_EX}{username}{Fore.LIGHTBLACK_EX} coins={Fore.RESET}{Fore.LIGHTWHITE_EX}{balance} coins {Fore.LIGHTCYAN_EX}||{Fore.RESET} {Fore.LIGHTBLACK_EX}infinity_founder={Fore.RESET}{str(has_infinity_founder).lower()} {Fore.LIGHTBLACK_EX}staff={Fore.RESET}{str(has_staff).lower()} {Fore.LIGHTBLACK_EX}developer={Fore.RESET}{str(has_developer).lower()} {Fore.LIGHTBLACK_EX}bug_hunter={Fore.RESET}{str(has_bug_hunter).lower()} {Fore.LIGHTBLACK_EX}premium={Fore.RESET}{str(has_premium).lower()}")
+            print(f"{Fore.LIGHTBLACK_EX}{current_time}{Fore.RESET} {Fore.LIGHTGREEN_EX}<+>{Fore.RESET} {Fore.RESET}{Fore.LIGHTWHITE_EX}User's info:{Fore.RESET}\n{Fore.LIGHTBLACK_EX}Username: {Fore.RESET}{Fore.LIGHTWHITE_EX}@{username}{Fore.LIGHTBLACK_EX}\n{Fore.LIGHTBLACK_EX}Description: {Fore.RESET}{Fore.LIGHTCYAN_EX}{description}{Fore.LIGHTBLACK_EX}\nCoins: {Fore.RESET}{Fore.LIGHTWHITE_EX}{balance} coins {Fore.LIGHTCYAN_EX}\n{Fore.LIGHTGREEN_EX}--- BADGES ---{Fore.RESET}{Fore.RESET}\n{Fore.LIGHTBLACK_EX}Infinity Founder={Fore.RESET}{str(has_infinity_founder).lower()}\n{Fore.LIGHTBLACK_EX}Staff={Fore.RESET}{str(has_staff).lower()}\n{Fore.LIGHTBLACK_EX}Developer={Fore.RESET}{str(has_developer).lower()}\n{Fore.LIGHTBLACK_EX}Bug Hunter={Fore.RESET}{str(has_bug_hunter).lower()}\n{Fore.LIGHTBLACK_EX}Premium={Fore.RESET}{str(has_premium).lower()}")
         else:
             print(f"{Fore.LIGHTBLACK_EX}{current_time}{Fore.RESET} {Fore.LIGHTGREEN_EX}<+>{Fore.RESET} {Fore.RESET} User is not token, meaning I don't have information on it! {Fore.LIGHTBLACK_EX}username={Fore.RESET}{username}")
     except requests.RequestException as e:
@@ -35,7 +39,6 @@ def parse_badge(html_text, badge_name):
     soup = BeautifulSoup(html_text, 'html.parser')
     badge_tags = soup.find_all('p', class_=lambda x: x and badge_name.lower() in x.lower())
     return bool(badge_tags)
-
 
 def check(username):
     try:
